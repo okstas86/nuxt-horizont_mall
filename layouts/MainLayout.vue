6a44a55761f2aaeb023758221465b6d09acb7546
okstas86 <stanislavocunev1@gmail.com>
2024-02-10 21:38:08 +0200

    Created store/user.js and added MainLayout, Loading and Footer  components

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -156,11 +156,15 @@
 	<div class="lg:pt-[150px] md:pt-[130px pt-[80px]" />
 
 	<slot />
+	<Footer v-if="!userStore.isLoading" />
 </template>
 
 <script setup>
+import { useUserStore } from "../store/user"
+const userStore = useUserStore()
+
 let isAccountMenu = ref(false)
 let isCartHover = ref(false)
 let isSearching = ref(false)
-let searchItem = ref('')
+let searchItem = ref("")
 </script>
